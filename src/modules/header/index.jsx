1076780403efb36f221ea60f27081a6d46d027b4
unknown <caiohbe@gmail.com>
2024-12-05 20:51:11 -0300

    feat(Header): modal foundation

--- src/modules/header/index.jsx
+++ src/modules/header/index.jsx
@@ -1,13 +1,70 @@
 import styled from "styled-components"
+import { useRef } from "react"
 
 export default function Header() {
+  const modalRef = useRef()
+
+  function openDialog() {
+    modalRef.current.showModal()
+  }
+
+  function closeDialog() {
+    modalRef.current.close()
+  }
+
   return (
     <HeaderContainer>
       <button>Hello World</button>
-      <button>+</button>
+      <button onClick={openDialog}>+</button>
+      <AddClothModal ref={modalRef}>
+        <button onClick={closeDialog}>Cancel</button>
+      </AddClothModal>
     </HeaderContainer>
   )
 }
+
+const AddClothModal = styled.dialog`
+  border: none;
+  border-radius: 2%;
+  padding: 1rem;
+  height: 80%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  width: min(80vw, 800px);
+
+  @media (width <= 450px) {
+    width: 100%;
+    border-radius: 0;
+  }
+
+  button {
+    transition: 200ms ease-in-out;
+    &:hover {
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+        0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      background-color: #b70821;
+      filter: brightness(95%);
+      cursor: pointer;
+    }
+    position: absolute;
+    bottom: 3%;
+    left: 5%;
+    font-size: 1rem;
+    color: white;
+    padding: 0.1rem 1rem;
+    border: none;
+    background-color: black;
+    border-radius: 12rem;
+    line-height: 1.5em;
+  }
+
+  /* img {
+    height: 40%;
+    border: 2px solid lime;
+    border-radius: 5%;
+    padding: 8px;
+  } */
+`
 
 const HeaderContainer = styled.div`
   display: flex;
